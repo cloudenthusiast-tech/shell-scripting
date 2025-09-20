2fee8c23a917e24225e7877f467cd6b9bac0eb61
Aravind <kolanuaravindreddy@gmail.com>
2025-09-20 15:18:06 +0530

    some commit

--- variable5.sh
+++ variable5.sh
@@ -3,7 +3,7 @@ FUMNAME="hakunamatat"
 echo "all variables passed to this script: $@"
 echo "all variables passed to this script: $*"
 echo "script name: $0"
-echo "pwd is : $pwd"
+echo "pwd is :$PWD"
 
 
 echo "this is the PID of this script: $$"
